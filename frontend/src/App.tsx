6b84662f04d3cc0e05a68a2517511509691dad35
MarvelousMarcin <01159356@pw.edu.pl>
2023-10-23 19:48:24 +0200

    styling init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,5 @@
-import "./App.css";
-
 function App() {
-  return (
-    <>
-      <h1>LARPex</h1>
-    </>
-  );
+  return <h1 className="text-3xl font-bold">LARPex</h1>;
 }
 
 export default App;
