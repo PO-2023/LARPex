c4580eb782dd743e311bd9520d6ba9ece788a692
Kuba072002 <jakub072002@gmail.com>
2023-11-06 21:21:50 +0100

    init backend

---
+++ backend/src/test/java/pw/edu/pl/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.edu.pl.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
