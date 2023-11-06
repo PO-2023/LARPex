46c5e64f7d2d10d1407eabc627baeceb8bca3693
Maciej Niszczota <macni.macni@gmail.com>
2023-11-07 00:20:04 +0100

    Add event date filtering method && tests

--- backend/src/test/java/pw/edu/pl/backend/EventServiceTest.java
+++ backend/src/test/java/pw/edu/pl/backend/EventServiceTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import pw.edu.pl.backend.service.EventService;
 
+import java.util.Date;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -15,5 +17,60 @@ public class EventServiceTest {
         var eventDtoList = eventService.getAllEvents();
         assertEquals(3, eventDtoList.size());
     }
+
+    @Test
+    public void getEventsExistingInTimeFrame(){
+        // Create a new Date object representing the current date and time
+        Date currentDate = new Date();
+
+        // Get the timestamp of the current date
+        long currentTime = currentDate.getTime();
+
+        // Calculate timestamps for the previous and next day
+        long oneDayInMillis = 24 * 60 * 60 * 1000; // 1 day in milliseconds
+        long previousDayTime = currentTime - oneDayInMillis;
+        long nextDayTime = currentTime + oneDayInMillis;
+
+        EventService eventService = new EventService();
+        var eventDtoList = eventService.getEvents(new Date(previousDayTime), new Date(nextDayTime));
+        assertEquals(3, eventDtoList.size());
+    }
+
+    @Test
+    public void getEventsNotExistingInFutureTimeFrame(){
+        // Create a new Date object representing the current date and time
+        Date currentDate = new Date();
+
+        // Get the timestamp of the current date
+        long currentTime = currentDate.getTime();
+
+        // Calculate timestamps for the previous and next day
+        long oneDayInMillis = 24 * 60 * 60 * 1000; // 1 day in milliseconds
+        long nextDayTime = currentTime + oneDayInMillis;
+        long afterNextDayTime = currentTime + 2 * oneDayInMillis;
+
+        EventService eventService = new EventService();
+        var eventDtoList = eventService.getEvents(new Date(nextDayTime), new Date(afterNextDayTime));
+        assertEquals(0, eventDtoList.size());
+    }
+
+    @Test
+    public void getEventsNotExistingInPastTimeFrame(){
+        // Create a new Date object representing the current date and time
+        Date currentDate = new Date();
+
+        // Get the timestamp of the current date
+        long currentTime = currentDate.getTime();
+
+        // Calculate timestamps for the previous and next day
+        long oneDayInMillis = 24 * 60 * 60 * 1000; // 1 day in milliseconds
+        long previousDayTime = currentTime - oneDayInMillis;
+        long beforePreviousDayTime = currentTime - 2 * oneDayInMillis;
+
+        EventService eventService = new EventService();
+        var eventDtoList = eventService.getEvents(new Date(beforePreviousDayTime), new Date(previousDayTime));
+        assertEquals(0, eventDtoList.size());
+    }
+
 
 }
