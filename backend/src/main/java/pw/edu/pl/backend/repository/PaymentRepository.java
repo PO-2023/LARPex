d8887b347f72d5fdc5a88a2c67aae03c8ff9f21e
zvzia <gaik.zuza@gmail.com>
2023-11-13 18:33:56 +0100

    payments

---
+++ backend/src/main/java/pw/edu/pl/backend/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package pw.edu.pl.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pw.edu.pl.backend.entity.PaymentEn;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<PaymentEn, Integer> {
+}
