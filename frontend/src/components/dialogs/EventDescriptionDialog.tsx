23a8e82c1e24431d1150e5398a9ea689b92a6037
Marcin Maciąg <01159356@pw.edu.pl>
2023-11-20 17:38:01 +0100

    connection fix

--- frontend/src/components/dialogs/EventDescriptionDialog.tsx
+++ frontend/src/components/dialogs/EventDescriptionDialog.tsx
@@ -1,35 +1,50 @@
-import {Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle,} from "@/components/ui/dialog";
-import {useDialog} from "@/stores/dialogStore/dialogStore";
-import {useSelectedEvent} from "@/stores/selectedItemStore/selectedItemStore";
-import {CalendarCheck, CalendarX, DollarSign, Swords, Users,} from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useDialog } from "@/stores/dialogStore/dialogStore";
+import { useSelectedEvent } from "@/stores/selectedItemStore/selectedItemStore";
+import {
+  CalendarCheck,
+  CalendarX,
+  DollarSign,
+  Swords,
+  Users,
+} from "lucide-react";
 
 const EventDescriptionDialog = () => {
-  const {closeDialog} = useDialog();
-  const {selectedEvent} = useSelectedEvent();
+  const { closeDialog } = useDialog();
+  const { selectedEvent } = useSelectedEvent();
 
   if (!selectedEvent) return;
 
+  const startTime = new Date(selectedEvent.startTime);
+  const endTime = new Date(selectedEvent.endTime);
+
   return (
-      <Dialog open={true} onOpenChange={closeDialog}>
-        <DialogContent className="min-h-[20rem] max-w-[19rem]">
-          <DialogHeader>
-            <DialogTitle>
+    <Dialog open={true} onOpenChange={closeDialog}>
+      <DialogContent className="min-h-[20rem] max-w-[19rem]">
+        <DialogHeader>
+          <DialogTitle>
             <span className="font-bold text-indigo-500 text-xl">
-              {selectedEvent?.eventName}
+              {selectedEvent?.name}
             </span>
-            </DialogTitle>
-            <DialogDescription className="mt-4">
-              Dane wydarzenia
-            </DialogDescription>
+          </DialogTitle>
+          <DialogDescription className="mt-4">
+            Dane wydarzenia
+          </DialogDescription>
           <section className="flex flex-col gap-1 text-sm">
             <div className="flex gap-3 mt-3 items-center">
               <Swords /> {selectedEvent?.game?.title}
             </div>
             <div className="flex gap-3 mt-3 items-center">
-              <CalendarCheck /> {selectedEvent?.startTime?.toLocaleDateString()}
+              <CalendarCheck /> {startTime?.toLocaleDateString()}
             </div>
             <div className="flex gap-3 mt-3 items-center">
-              <CalendarX /> {selectedEvent?.endTime?.toLocaleDateString()}
+              <CalendarX /> {endTime?.toLocaleDateString()}
             </div>
             <div className="flex gap-3 mt-3 items-center">
               <Users /> {selectedEvent.players}/{selectedEvent.game.playerLimit}
