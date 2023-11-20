622d813e41754c0c9f0f4d6c5c08b63f74332c46
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2023-11-20 18:52:42 +0100

    Fetch events from remote

--- frontend/src/usecase/event/IDisplayEventList.ts
+++ frontend/src/usecase/event/IDisplayEventList.ts
@@ -3,19 +3,36 @@ import {IEventsPresenter} from "@/class/presenter/EventsPresenter";
 import axios from "axios";
 
 export interface IDisplayEventList {
-  getEvents(dateRange: DateRange | undefined): any;
+    getEvents(dateRange: DateRange | undefined): any;
 }
 
 export class DisplayEventList implements IDisplayEventList {
-  constructor(private presenter: IEventsPresenter) {
-  }
+    constructor(private presenter: IEventsPresenter) {
+    }
 
-  async getEvents() {
-    this.presenter.startLoading();
-    try {
-      const { data } = await axios.get("http://localhost:8080/event");
-      console.log(data);
-      this.presenter.setEvents(data);
-    } catch (e) {}
-  }
+    async getEvents(dateRange: DateRange | undefined) {
+        this.presenter.startLoading();
+        try {
+            let dateQuery = ""
+            if (dateRange?.from) {
+                dateQuery = `?dateFrom=${this.formatDateToYYYYMMDD(dateRange.from)}`
+                if (dateRange.to) {
+                    dateQuery += `&dateTo=${this.formatDateToYYYYMMDD(dateRange.to)}`
+                } else {
+                    dateQuery += `&dateTo=${this.formatDateToYYYYMMDD(dateRange.from)}`
+                }
+            }
+            const {data} = await axios.get(`http://localhost:8080/event${dateQuery}`);
+            this.presenter.setEvents(data);
+        } catch (e) {
+        }
+    }
+
+    formatDateToYYYYMMDD(date) {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+        const day = String(date.getDate()).padStart(2, '0');
+
+        return `${year}-${month}-${day}`;
+    }
 }
