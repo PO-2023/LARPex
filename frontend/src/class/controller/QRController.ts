79a0b34a265d8b7fee25715bc5d5dba03535a2ac
Michal Ankiersztajn <michalankiersztajn@gmail.com>
2024-01-07 15:57:25 +0100

    Fix multi requests from QR Code

--- frontend/src/class/controller/QRController.ts
+++ frontend/src/class/controller/QRController.ts
@@ -4,12 +4,17 @@ import {ActionDTO} from "@/class/dto/qr/ActionDTO";
 export class QRController {
     constructor(private interactWithQR: IInteractWithQR) {
     }
+    isHandled: Boolean = false
 
     onResult(result: string | undefined) {
+        if (this.isHandled) {
+            return
+        }
+        this.isHandled = true
         if (!!result) {
             try {
                 const qrData = JSON.parse(result)
-                this.interactWithQR.interact(new ActionDTO(qrData.actionType, qrData.itemId, qrData.itemQuantity, 2,qrData.doorId))
+                this.interactWithQR.interact(new ActionDTO(qrData.actionType, qrData.itemId, qrData.itemQuantity, 2, qrData.doorId))
             } catch (e) {
                 this.interactWithQR.handleError("Błąd zczytywania kodu QR")
             }
